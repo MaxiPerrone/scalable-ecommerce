127d55837f7106e2ce9a7df2ea43993f809a4b8f
MaxiPerrone <maximiliano@rewardsweb.com>
2024-11-30 10:34:56 -0300

    Read server configs from item service

--- items/src/main/java/com/springcloud/ecommerce/items/controller/ItemController.java
+++ items/src/main/java/com/springcloud/ecommerce/items/controller/ItemController.java
@@ -5,19 +5,35 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springcloud.ecommerce.items.service.ItemService;
 import com.springcloud.ecommerce.items.view.ItemResponse;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 public class ItemController {
 
     private final ItemService service;
 
+    @Autowired
+    private Environment environment;
+
     public ItemController(@Qualifier("itemServiceWebClient") ItemService service) {
         this.service = service;
     }
+
+    @GetMapping("/configs")
+    public ResponseEntity<?> configs() {
+        var config = new HashMap<>();
+        config.put("description", environment.getProperty("app.configuration.description"));
+        return ResponseEntity.ok(config);
+    }
+    
 
     @GetMapping
     public ResponseEntity<?> list() {
